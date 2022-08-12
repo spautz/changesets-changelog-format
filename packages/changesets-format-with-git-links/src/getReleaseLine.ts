644a7e0cd016b25e202b6dfdb69165f99b933ab4
Steven Pautz <spautz@gmail.com>
2022-08-11 22:20:42 -0400

    Placeholder in setting up options and formatting

--- packages/changesets-format-with-git-links/src/getReleaseLine.ts
+++ packages/changesets-format-with-git-links/src/getReleaseLine.ts
@@ -3,6 +3,22 @@ import { NewChangesetWithCommit, VersionType } from '@changesets/types';
 import { findCommitForChangeset } from './findCommitForChangeset';
 import { Options, defaultOptions } from './options';
 
+// @TODO: typings
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const processTemplate = (template: string | undefined, _data: any): string => {
+  if (!template) {
+    return '';
+  } else if (!template.includes('$')) {
+    return template;
+  } else {
+    // Replace any vars (excluding `\$`) with their data
+    return template.replace(/(!?\\\$)\$/g, (...args) => {
+      console.log('replacer', ...args);
+      return args[0];
+    });
+  }
+};
+
 const getReleaseLine = async (
   changeset: NewChangesetWithCommit,
   _type: VersionType,
@@ -14,11 +30,33 @@ const getReleaseLine = async (
         ...options,
       }
     : defaultOptions;
+  const { commitTemplate, commitMissingTemplate, issueTemplate, issueMissingTemplate } =
+    optionsWithDefaults;
+
   const commitInfo = await findCommitForChangeset(changeset, optionsWithDefaults);
 
   const [firstLine, ...futureLines] = changeset.summary.split('\n').map((l) => l.trimEnd());
 
-  let returnVal = `- ${commitInfo ? `[${commitInfo.abbrevHash}]: ` : ''}${firstLine}`;
+  // @TODO: typings
+  const templateData = {
+    ...optionsWithDefaults,
+    ...commitInfo,
+  };
+
+  let returnVal = `- ${firstLine}  ${commitInfo ? `[${commitInfo.abbrevHash}]: ` : ''}`;
+  if (commitInfo) {
+    // Append issue, if present
+    if (commitInfo.issueNum) {
+      returnVal += processTemplate(issueTemplate, templateData);
+    } else {
+      returnVal += processTemplate(issueMissingTemplate, templateData);
+    }
+
+    // Append commit, if present
+    returnVal += processTemplate(commitTemplate, templateData);
+  } else {
+    returnVal += processTemplate(commitMissingTemplate, templateData);
+  }
 
   if (futureLines.length > 0) {
     returnVal += `\n${futureLines.map((l) => `  ${l}`).join('\n')}`;
