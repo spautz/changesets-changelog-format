0a8a6d8b5be06e2cbb5f246c379f2b33e4c7fd91
Steven Pautz <spautz@gmail.com>
2022-08-11 22:20:17 -0400

    First well-formed build

--- packages/changesets-format-with-git-links/src/findRepoRoot.ts
+++ packages/changesets-format-with-git-links/src/findRepoRoot.ts
@@ -1,10 +1,11 @@
 import path from 'node:path';
-import { findUp, pathExists } from 'find-up';
 
 /**
  * Starting from the current directory, hronously walks up until it finds the `.git` directory.
  */
 const findRepoRoot = async (): Promise<string> => {
+  const { findUp, pathExists } = await import('find-up');
+
   const repoRoot = await findUp(
     async (directory) => {
       const hasGitDir = await pathExists(path.join(directory, '.git'));
